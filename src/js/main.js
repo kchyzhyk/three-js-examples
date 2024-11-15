569e9d32811b37d31f6907943da2cc8d8a728c54
kchyzhyk <kchyzhykova888@gmail.com>
2024-11-15 19:25:18 +0100

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
 import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 import * as lil from 'lil-gui'
 
+// 
 // const scene = new THREE.Scene();
 // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // const renderer = new THREE.WebGLRenderer({ antialias: true });
